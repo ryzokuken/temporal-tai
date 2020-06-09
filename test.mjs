7b669524495fd31ef7979be5eb74893a51fc0bdd
Ujjwal Sharma <ryzokuken@disroot.org>
2020-06-09 23:53:06 +0530

    tests: use numerical separators for clarity

--- test.mjs
+++ test.mjs
@@ -12,7 +12,7 @@ const dt2 = Temporal.DateTime.from('1973-01-01T00:00:00');
 assert.equal(dt1.inTimeZone('UTC').difference(dt2.inTimeZone('UTC')).toString(), 'PT31622400S');
 assert.equal(dt1.inTimeZone(tai, disambiguation).difference(dt2.inTimeZone(tai, disambiguation)).toString(), 'PT31622402S');
 
-const abs1 = new Temporal.Absolute(915148831000n * 1000000n);
-const abs2 = new Temporal.Absolute(915148832000n * 1000000n);
+const abs1 = new Temporal.Absolute(915148831000n * 1_000_000n);
+const abs2 = new Temporal.Absolute(915148832000n * 1_000_000n);
 assert.notEqual(abs1.inTimeZone('UTC').toString(), abs2.inTimeZone('UTC').toString());
 assert.equal(abs1.inTimeZone(tai).toString(), abs2.inTimeZone(tai).toString());
